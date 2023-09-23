d96e36ced8a31859d10c025e2050515e979ee406
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-09-23 10:15:47 -0700

    Create book display function+remove some options

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
 const myLibrary = [];
 const addButton = document.querySelector("#add");
+const bookHolder = document.querySelector(".books")
 
 addButton.addEventListener("click", addBookToLibrary);
 
-function Book(author, title, pages, genre, year, read) {
+function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
-    this.genre = genre;
-    this.year = year;
     this.read = read;
 }
 
 function addBookToLibrary() {
-    let newBook = new Book(prompt("Author?"), prompt("Title?"), prompt("Pages?"), prompt("Genre?"), prompt("Release Date?"), prompt("Read or not?"))
+    let newBook = new Book(prompt("Author?"), prompt("Title?"), prompt("Pages?"), prompt("Read or not?"));
     myLibrary.push(newBook);
+    displayBooks();
+}
+
+function displayBooks() {
+    bookHolder.replaceChildren();
+    myLibrary.forEach((book) => {
+        let displayedBook = document.createElement("div");
+        bookHolder.appendChild(displayedBook);
+        displayedBook.classList.add("book");
+
+        let bookInfo = document.createElement("div");
+        displayedBook.appendChild(bookInfo);
+        bookInfo.classList.add("book-info");
+        bookInfo.innerText = `Author: ${book.author}\nTitle: ${book.title}\nPages: ${book.pages}\nRead: ${book.read}`;
+    })
 }
 
 
